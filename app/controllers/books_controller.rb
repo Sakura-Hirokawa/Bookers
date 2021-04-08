c44e835bd24085d98b884f64e8a82a35904f68a2
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-08 07:31:56 +0000

    [Add]index,new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -7,7 +9,18 @@ class BooksController < ApplicationController
 
   def new
   end
+  
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
